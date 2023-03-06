1b66b2363816084a1064854292d2abe8799e0227
Roman <kolomrs@yandex.ru>
2023-03-06 21:59:57 +0300

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import NoteList from './components/NoteList';
-import NoteForm from './components/NoteForm';
-import NoteEdit from './components/NoteEdit';
-import './App.css';
-import { getNotes, saveNotes } from './utils/notes.js';
+import React, { useState, useEffect } from 'react'
+import NoteList from './components/NoteList'
+import NoteForm from './components/NoteForm'
+import NoteEdit from './components/NoteEdit'
+import './App.css'
+import { getNotes, saveNotes } from './utils/notes.js'
 
 const App = () => {
-    const [notes, setNotes] = useState([]);
-    const [editingNoteId, setEditingNoteId] = useState(null);
+  const [notes, setNotes] = useState([])
+  const [editingNoteId, setEditingNoteId] = useState(null)
 
-    useEffect(() => {
-        setNotes(getNotes());
-    }, []);
+  useEffect(() => {
+    setNotes(getNotes())
+  }, [])
 
-    const handleAddNote = (text) => {
-        const newNote = {
-            id: Date.now(),
-            text: text,
-        };
-        setNotes([...notes, newNote]);
-        saveNotes([...notes, newNote]);
-    };
+  const handleAddNote = text => {
+    const newNote = {
+      id: Date.now(),
+      text: text,
+    }
+    setNotes([...notes, newNote])
+    saveNotes([...notes, newNote])
+  }
 
-    const handleEditNote = (id) => {
-        setEditingNoteId(id);
-    };
+  const handleEditNote = id => {
+    setEditingNoteId(id)
+  }
 
-    const handleSaveNote = (id, text) => {
-        const updatedNotes = notes.map((note) =>
-            note.id === id ? { ...note, text } : note
-        );
-        setNotes(updatedNotes);
-        saveNotes(updatedNotes);
-        setEditingNoteId(null);
-    };
+  const handleSaveNote = (id, text) => {
+    const updatedNotes = notes.map(note => (note.id === id ? { ...note, text } : note))
+    setNotes(updatedNotes)
+    saveNotes(updatedNotes)
+    setEditingNoteId(null)
+  }
 
-    const handleCancelEdit = () => {
-        setEditingNoteId(null);
-    };
+  const handleCancelEdit = () => {
+    setEditingNoteId(null)
+  }
 
-    const handleDeleteNote = (id) => {
-        const updatedNotes = notes.filter((note) => note.id !== id);
-        setNotes(updatedNotes);
-        saveNotes(updatedNotes);
-    };
+  const handleDeleteNote = id => {
+    const updatedNotes = notes.filter(note => note.id !== id)
+    setNotes(updatedNotes)
+    saveNotes(updatedNotes)
+  }
 
-    return (
-        <div className="App">
-            <h1>Notes App</h1>
-            {editingNoteId ? (
-                <NoteEdit
-                    id={editingNoteId}
-                    text={notes.find((note) => note.id === editingNoteId).text}
-                    onSave={handleSaveNote}
-                    onCancel={handleCancelEdit}
-                />
-            ) : (
-                <NoteForm onAdd={handleAddNote} />
-            )}
-            <NoteList
-                notes={notes}
-                onEdit={handleEditNote}
-                onDelete={handleDeleteNote}
-            />
-        </div>
-    );
-};
+  return (
+    <div className="App">
+      <h1>Notes App</h1>
+      {editingNoteId ? (
+        <NoteEdit
+          id={editingNoteId}
+          text={notes.find(note => note.id === editingNoteId).text}
+          onSave={handleSaveNote}
+          onCancel={handleCancelEdit}
+        />
+      ) : (
+        <NoteForm onAdd={handleAddNote} />
+      )}
+      <NoteList notes={notes} onEdit={handleEditNote} onDelete={handleDeleteNote} />
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import Note from './Note';
+import React from 'react'
+import Note from './Note'
 
 const NoteList = ({ notes, onEdit, onDelete }) => {
-    return (
-        <div className="note-list">
-            {notes.map((note) => (
-                <Note
-                    key={note.id}
-                    id={note.id}
-                    text={note.text}
-                    onEdit={onEdit}
-                    onDelete={onDelete}
-                />
-            ))}
-        </div>
-    );
-};
+  return (
+    <div className="note-list">
+      {notes.map(note => (
+        <Note key={note.id} id={note.id} text={note.text} onEdit={onEdit} onDelete={onDelete} />
+      ))}
+    </div>
+  )
+}
 
-export default NoteList;
+export default NoteList
